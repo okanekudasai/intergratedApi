cfe2c740f255a87dc6aadbdd6b4a5d1396679890
okane <okanekudasai6@gmail.com>
2024-04-25 20:18:10 +0900

    init

--- src/main/java/com/okane/intergratedapi/testing/TestController.java
+++ src/main/java/com/okane/intergratedapi/testing/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     String getTest() {
-        return "Hello, this is a test response!!!!";
+        return "hello intergrated";
     }
 }

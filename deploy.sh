423a97e25568cf319b5b5d5e899826851c2d723c
okane <okanekudasai6@gmail.com>
2024-05-08 10:56:16 +0900

    init

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@
          kill -9 $CURRENT_PID
          sleep 3
  fi
- echo "> 새로운 앱을 배포할게요"
+ echo "> 새로운 앱을 배포할게요!"
 
  cd /home/ubuntu/deploy
  JAR_NAME=$(ls | grep -v '\-plain' | grep '.jar$' | tail -n 1) #jar파일의 이름을 정규표현식같은거로 찾는거 여기서는 -plain이 안들어있는 .jar파일을 찾느다

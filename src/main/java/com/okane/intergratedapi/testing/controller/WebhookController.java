949afef5608dee28e2dd698263342612f034469c
okane <okanekudasai6@gmail.com>
2024-05-27 23:16:31 +0900

    webhooktest

--- src/main/java/com/okane/intergratedapi/testing/controller/WebhookController.java
+++ src/main/java/com/okane/intergratedapi/testing/controller/WebhookController.java
@@ -57,14 +57,14 @@ public class WebhookController {
         JsonElement author = commit.getAsJsonObject().get("author").getAsJsonObject();
         String authorName = author.getAsJsonObject().get("name").getAsString();
         String authorUserName = author.getAsJsonObject().get("username").getAsString();
-        String added = commit.getAsJsonObject().get("added").getAsString();
-        String problem = added;
+        JsonArray added = commit.getAsJsonObject().get("added").getAsJsonArray();
+        String problem = added.get(0).getAsString();
         StringBuilder sb = new StringBuilder();
         sb.append("URL : ").append(url).append("\n");
         sb.append("저자이름 : ").append(authorName).append("\n");
         sb.append("닉네임 : ").append(authorUserName).append("\n");
         sb.append("문제 : ").append(problem).append("\n");
-        System.out.println(payload);
+//        System.out.println(payload);
         System.out.println(sb);
         // 원하는 비즈니스 로직을 여기에 추가합니다.
         return ResponseEntity.ok("Success");

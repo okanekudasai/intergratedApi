2a988c67dc68ff09b6dd9dddc702f0164786f247
okane <okanekudasai6@gmail.com>
2024-05-27 22:34:18 +0900

    webhooktest

--- src/main/java/com/okane/intergratedapi/testing/controller/WebhookController.java
+++ src/main/java/com/okane/intergratedapi/testing/controller/WebhookController.java
@@ -1,5 +1,9 @@
 package com.okane.intergratedapi.testing.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.binary.Hex;
@@ -16,6 +20,8 @@ import java.util.Map;
 @RequestMapping("/webhook")
 @RequiredArgsConstructor
 public class WebhookController {
+
+    ObjectMapper mapper = new ObjectMapper();
 
     @Value("${github.webhook.secret}")
     String GITHUB_SECRET;
@@ -43,7 +49,21 @@ public class WebhookController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid signature");
         }
         // GitHub Webhook에서 보내는 페이로드 처리
-        System.out.println("Received payload: " + payload);
+        JsonElement element = JsonParser.parseString(payload);
+        JsonElement repository = element.getAsJsonObject().get("repository").getAsJsonObject();
+        String blobUrl = repository.getAsJsonObject().get("blobs_url").getAsString();
+        JsonElement commit = element.getAsJsonObject().get("head_commit").getAsJsonObject();
+        JsonElement author = commit.getAsJsonObject().get("author").getAsJsonObject();
+        String authorName = author.getAsJsonObject().get("name").getAsString();
+        String authorUserName = author.getAsJsonObject().get("username").getAsString();
+        JsonArray added = commit.getAsJsonObject().get("added").getAsJsonArray();
+        String problem = added.get(0).toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("URL : ").append(blobUrl).append("\n");
+        sb.append("저자이름 : ").append(authorName).append("\n");
+        sb.append("닉네임 : ").append(authorUserName).append("\n");
+        sb.append("문제 : ").append(problem).append("\n");
+        System.out.println(sb);
         // 원하는 비즈니스 로직을 여기에 추가합니다.
         return ResponseEntity.ok("Success");
     }

00728653885ad349ebd3271f4370cd09007260a4
okane <okanekudasai6@gmail.com>
2024-05-28 06:05:48 +0900

    webhooktest

--- src/main/java/com/okane/intergratedapi/testing/controller/WebhookController.java
+++ src/main/java/com/okane/intergratedapi/testing/controller/WebhookController.java
@@ -56,6 +56,40 @@ public class WebhookController {
     }
 
     /**
+     * 오후 4시가 되면 자동으로 작동하는 코드
+     */
+    @GetMapping("/confirmDaily")
+//    @Scheduled(cron = "0 0 16 * * *")
+    void confirmDaily() {
+
+        String reqURL = "https://leetcode.com/graphql";
+        String body = "{\"query\":\"query questionOfToday {\\n\\tactiveDailyCodingChallengeQuestion {\\n\\t\\tdate\\n\\t\\tuserStatus\\n\\t\\tlink\\n\\t\\tquestion {\\n\\t\\t\\tacRate\\n\\t\\t\\tdifficulty\\n\\t\\t\\tfreqBar\\n\\t\\t\\tfrontendQuestionId: questionFrontendId\\n\\t\\t\\tisFavor\\n\\t\\t\\tpaidOnly: isPaidOnly\\n\\t\\t\\tstatus\\n\\t\\t\\ttitle\\n\\t\\t\\ttitleSlug\\n\\t\\t\\thasVideoSolution\\n\\t\\t\\thasSolution\\n\\t\\t\\ttopicTags {\\n\\t\\t\\t\\tname\\n\\t\\t\\t\\tid\\n\\t\\t\\t\\tslug\\n\\t\\t\\t}\\n\\t\\t}\\n\\t}\\n}\\n\",\"operationName\":\"questionOfToday\"}";
+
+        WebClient webClient = WebClient.builder()
+                .baseUrl(reqURL)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+        String response = webClient.post()
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        JsonElement element = JsonParser.parseString(response);
+        JsonElement problem_data = element.getAsJsonObject().get("data").getAsJsonObject().get("activeDailyCodingChallengeQuestion").getAsJsonObject();
+        String date = problem_data.getAsJsonObject().get("date").getAsString();
+        String link = problem_data.getAsJsonObject().get("link").getAsString();
+        String number = problem_data.getAsJsonObject().get("question").getAsJsonObject().get("frontendQuestionId").getAsString();
+        String title = problem_data.getAsJsonObject().get("question").getAsJsonObject().get("titleSlug").getAsString();
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("날짜 : ").append(date).append("\n");
+//        sb.append("링크 : ").append("https://leetcode.com" + link).append("\n");
+//        sb.append("숫자 : ").append(number + "-" + title).append("\n");
+//        System.out.println(sb);
+        makeNewLine(number + "-" + title, date, "https://leetcode.com" + link);
+    }
+
+    /**
      * 깃허브에 푸시가 되면 자동으로 실행되는 메서드,, 깃허브에 변화가 생기면 노셔닝함수를 호출하여 노션데이터베이스의 정보를 수정한다.
      * @param payload
      * @param signature
@@ -102,7 +136,7 @@ public class WebhookController {
         //        System.out.println(payload);
 //        System.out.println(sb);
 
-        System.out.println(authorName);
+//        System.out.println(authorName);
         if (edited.endsWith(".md")) {
             System.out.println("md파일 스킵");
             return ResponseEntity.ok("Skip md");
@@ -153,14 +187,15 @@ public class WebhookController {
     }
 
     @GetMapping("/makeNewLine")
-    private String makeNewLine(String problem) {
+    private String makeNewLine(String problem, String date, String link) {
 
-        LocalDate today = LocalDate.now();
-        String formattedDate = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+//        LocalDate today = LocalDate.now();
+//        String formattedDate = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 //        System.out.println("오늘 날짜: " + formattedDate);
+        String formattedDate = date;
 
         String reqURL = "https://api.notion.com/v1/pages";
-        String body = "{\"parent\":{\"type\":\"database_id\",\"database_id\":\"" + database_id + "\"},\"properties\":{\"날짜\":{\"date\":{\"start\":\"" + formattedDate + "\"}},\"문제\":{\"title\":[{\"text\":{\"content\":\"" + problem + "\",\"link\":null}}]},\"이동하\":{\"url\":\" \"},\"박아멘\":{\"url\":\" \"}}}";
+        String body = "{\"parent\":{\"type\":\"database_id\",\"database_id\":\"" + database_id + "\"},\"properties\":{\"링크\":{\"url\":\"" + link + "\"},\"날짜\":{\"date\":{\"start\":\"" + formattedDate + "\"}},\"문제\":{\"title\":[{\"text\":{\"content\":\"" + problem + "\",\"link\":null}}]},\"이동하\":{\"url\":\" \"},\"박아멘\":{\"url\":\" \"}}}";
 
         WebClient webClient = WebClient.builder()
                 .baseUrl(reqURL)
@@ -211,7 +246,7 @@ public class WebhookController {
             }
         }
         System.out.println("못찾음");
-        return makeNewLine(problem);
+        return null;
 
         //가져온 열에서 problem과 같은 문제가 있는지 확인
         //있으면 그 열의 id 반환
@@ -224,6 +259,10 @@ public class WebhookController {
 //        String target_url = "https://github.com/okanekudasai/leethub_notion_connector/blob/master/0001-two-sum/0001-two-sum.java";
 
         String target_id = notionQuery(problem_name);
+        if (target_id == null) {
+            System.out.println("해당 문제는 데일리 문제가 아님");
+            return;
+        }
         if (target_id.charAt(0) == '"') target_id = target_id.substring(1, target_id.length() - 1);
 //        System.out.println(target_id);
         String reqURL = "https://api.notion.com/v1/pages/" + target_id;
